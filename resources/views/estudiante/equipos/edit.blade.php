ff0e24eb801bb8cfcfc5af752c5ddb70a650a04e
Gael Jafet Aquino Cruz <gaeljac7799@gmail.com>
2025-12-05 12:06:39 -0600

    bton

--- resources/views/estudiante/equipos/edit.blade.php
+++ resources/views/estudiante/equipos/edit.blade.php
@@ -255,13 +255,13 @@
 
 <div class="editar-equipo-page py-12">
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+        <a href="{{ route('estudiante.equipo.show-detalle', $equipo) }}" class="back-link">
+            <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
+            </svg>
+            Volver a {{ $equipo->nombre }}
+        </a>
         <div class="flex items-center mb-6">
-            <a href="{{ route('estudiante.equipo.index') }}" class="back-link">
-                <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
-                </svg>
-                Volver al equipo
-            </a>
             <h2 class="font-semibold text-xl ml-2">
                 Editar Equipo: {{ $equipo->nombre }}
             </h2>
